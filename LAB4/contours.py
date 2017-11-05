08454e4e212780983baf03d8dce9e4734de19403
jakub-tomczak <jakub.tomczak@live.com>
2017-11-05 19:04:47 +0100

    Remove noises.

--- LAB4/contours.py
+++ LAB4/contours.py
@@ -2,15 +2,40 @@ from __future__ import division
 from pylab import *
 from skimage import io, filters
 import skimage.morphology as mp
+import skimage.measure as measure
 from skimage.color import rgb2gray
 
 import numpy as np
 from ipykernel.pylab.backend_inline import flush_figures
 
+def hsv2rgb(h, s, v):
+    import math
+    h = float(h)
+    s = float(s)
+    v = float(v)
+    h60 = h / 60.0
+    h60f = math.floor(h60)
+    hi = int(h60f) % 6
+    f = h60 - h60f
+    p = v * (1 - s)
+    q = v * (1 - f * s)
+    t = v * (1 - (1 - f) * s)
+    r, g, b = 0, 0, 0
+    if hi == 0: r, g, b = v, t, p
+    elif hi == 1: r, g, b = q, v, p
+    elif hi == 2: r, g, b = p, v, t
+    elif hi == 3: r, g, b = p, q, v
+    elif hi == 4: r, g, b = t, p, v
+    elif hi == 5: r, g, b = v, p, q
+    return [r, g, b]
+def lerp(start, stop, value):
+    return start + (stop - start)*value
+
 def contourDetector(image, threshold = .15):
     gray = rgb2gray(image)
     sobelImage = filters.sobel(gray)
     thresold = mp.dilation(colorThreshold(sobelImage, getMax(sobelImage)*threshold))
+    #out = measure.label(thresold, background=0, neighbors=8, return_num=True)
     return thresold
 
 def processAll(threshold = .15):
@@ -19,10 +44,34 @@ def processAll(threshold = .15):
     contours = [contourDetector(image) for image in images]
     displaySaveImage(contours, "planes_t{}.png".format(threshold))
 
+
 def processOne(number):
     filename = "samolot%02d" % number
     image = readImage(filename)
-    displaySaveImage([contourDetector(image)], filename)
+    contour = contourDetector(image)
+    labeled, num = measure.label(contour, background=0, neighbors=8, return_num=True)
+    colored = np.zeros([len(labeled), len(labeled[0]), 3])
+    imSize = len(labeled) * len(labeled[0])
+
+    unique, counts = np.unique(labeled, return_counts=True)
+    values = dict(zip(unique, counts))
+    print(len(labeled), len(labeled[0]))
+    labeled = [[ (values[value] > imSize*0.001)*value for value in row] for row in labeled]
+
+    unique, counts = np.unique(labeled, return_counts=True)
+    values = dict(zip(unique, counts))
+    print(values)
+
+
+
+    for row in range(0, len(labeled)):
+        for i in range(0, len(labeled[row])):
+            if labeled[row][i] != 0:
+                colored[row][i] = hsv2rgb(lerp(0, 360, labeled[row][i]/num), 1, 1)
+    #imColor = [[rank[labeled[row][value]] for value in range(0, len(row))] for row in range(0, len(labeled))]
+    print("Number of labels {}".format(num))
+
+    displaySaveImage([colored], "one/"+filename, resolution=100)
 
 def colorThreshold(image, t):
     processed = (image > t) * 1
@@ -48,7 +97,8 @@ def displaySaveImage(imgs, filename = "planes.png", resolution = 500):
     fig.savefig("out/"+filename, dpi=resolution)
 
 def main():
-    processAll()
-
+    #processAll()
+    #processOne(13)
+    [processOne(num) for num in range(0,21)]
 if __name__ == "__main__":
     main()
